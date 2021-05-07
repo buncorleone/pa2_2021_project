6b8561aade95b586a4ac9c15d43bd338dc49c28a
Peter McGrath <D19124766@mytudublin.com>
2021-05-07 15:04:05 +0100

    Commit on 07/05/2021. Login hard coded but working. Creating new post working.

--- Blogger/urls.py
+++ Blogger/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('', views.index),
-    path('myposts/', views.my_posts),
-    path('posts/', views.list_posts),
-    path('categories/', views.list_categories),
+    path('myposts/', views.my_posts, name='my_posts'),
+    path('posts/', views.Posts.as_view(), name='posts'),
+    path('categories/', views.Categories.as_view(), name='categories'),
+    path('register/', views.Register.as_view(), name='register_user'),
+    path('login/', TokenObtainPairView.as_view(), name='login_user'),
+    path('refreshlogin', TokenRefreshView.as_view(), name='refresh_login'),
 ]
--- Blogger/views.py
+++ Blogger/views.py
@@ -2,13 +2,16 @@ from django.contrib.auth import get_user_model
 # Temporary HttpResponse import used for testing, to be removed once GUI is done and views point to that.
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from django.contrib.auth import get_user_model
 from . import models
+from . import serializers
+import json
 
 
 def index(request):
@@ -27,8 +30,10 @@ def my_posts(request):
     return JsonResponse(returning_json, status=200)
 
 
-def list_posts(request):
-    if request.method == 'GET':
+class Posts(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
         posts = models.Post.objects.all()
 
         returning_json = {"posts": []}
@@ -36,13 +41,17 @@ def list_posts(request):
             returning_json["posts"].append(post.as_dict)
 
         return JsonResponse(returning_json, status=200)
-    if request.method == 'POST':
-        content = request.body
-        pst_title = content.value['title']
-        pst_cat = content.value['category']
-        pst_body = content.value['body']
-        # pst_auth = get_user_model()
-        return JsonResponse(pst_title, status=200)
+
+    def post(self, request):
+        content = request.data
+        author = request.user
+        models.Post.objects.create(
+            author=author,
+            title=content['title'],
+            category=models.Category.objects.filter(name=content["category"]).first(),
+            body=content['body']
+        )
+        return JsonResponse(content, status=200)
 
 
 # def create_post(request):
@@ -54,12 +63,72 @@ def list_posts(request):
 #         pst_auth = get_user_model()
 
 
-def list_categories(request):
-    categories = models.Category.objects.all()
+class Categories(APIView):
+    def get(self, request):
+        categories = models.Category.objects.all()
 
-    returning_json = {"categories": []}
-    for category in categories:
-        returning_json["categories"].append(category.as_dict)
+        returning_json = {"categories": []}
+        for category in categories:
+            returning_json["categories"].append(category.as_dict)
 
-    return JsonResponse(returning_json, status=200)
+        return JsonResponse(returning_json, status=200)
 
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
+
+# All code below was taken from wk11_drf_demo-master
+class Register(APIView):
+    """
+    Allows a new user to register an account.
+    """
+
+    def post(self, request):
+        """
+        Takes the following JSON structure as input:
+
+        {"username": "string", "password": "string", "first_name": "string", "last_name": "string", "is_admin": "boolean"}            }
+
+        """
+        try:
+            if not request.data:
+                raise ValueError("Invalid User data")
+
+            # serialized_data = serializers.RegisterSerializer(data=request.data)
+            # if serialized_data.is_valid(raise_exception=True):
+            incoming_data = {
+                "username": request.data["username"],
+                "password": request.data["password"],
+                "first_name": request.data["first_name"],
+                "last_name": request.data["last_name"],
+                "email": f"{request.data['first_name']}.{request.data['last_name']}@tudublin.ie",
+                "is_staff": request.data["is_admin"],
+            }
+            new_user = get_user_model().objects.create_user(**incoming_data)
+
+            return Response({
+                "username": request.data["username"],
+                "first_name": request.data["first_name"],
+                "last_name": request.data["last_name"],
+                "is_admin": request.data["is_admin"],
+            }, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            return Response({"error": f"{e}"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserMeDetails(generics.RetrieveAPIView):
+    """"
+    Retrieves a summarized view of the current User object.
+    """
+    serializer_class = serializers.UserMeSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+
+    def get_object(self):
+        return self.request.user
